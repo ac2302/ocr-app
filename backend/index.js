3f80a351ec2bf3fa1490f73084e459e1f67dbaf0
Aryan Chavan <aryan@aryansdomain.com>
2022-09-26 12:08:02 +0530

    replace capacitor react app with simple static frontend

--- backend/index.js
+++ backend/index.js
@@ -28,47 +28,47 @@ const port = 5050;
 app.listen(port, () => console.log(`App is listening on port ${port}.`));
 
 app.post("/ocr", async (req, res) => {
-	// try {
-	if (!req.files) {
-		res.send({
-			status: false,
-			message: "No file uploaded",
-		});
-	} else {
-		//Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
-		let uploadedFile = req.files.file;
-
-		const randomString =
-			(Math.random() + 1).toString(36).substring(7) +
-			(Math.random() + 1).toString(36).substring(7) +
-			(Math.random() + 1).toString(36).substring(7) +
-			(Math.random() + 1).toString(36).substring(7) +
-			(Math.random() + 1).toString(36).substring(7);
-
-		//Use the mv() method to place the file in upload directory (i.e. "uploads")
-		const path = `./uploads/${randomString}-${uploadedFile.name}`;
-		await uploadedFile.mv(path);
-
-		// scheduling delete
-		setTimeout(() => {
-			fs.rmSync(path);
-		}, 1000 * 5);
-
-		// ocr
-		tesseract
-			.recognize(path, { lang: "eng" })
-			.then((text) => {
-				console.log("Result:", text);
-				res.json({ text });
-			})
-			.catch((error) => {
-				console.log(error.message);
-				res.status(500).json({ msg: error.message });
+	try {
+		if (!req.files) {
+			res.send({
+				status: false,
+				message: "No file uploaded",
 			});
+		} else {
+			//Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
+			let uploadedFile = req.files.file;
+
+			const randomString =
+				(Math.random() + 1).toString(36).substring(7) +
+				(Math.random() + 1).toString(36).substring(7) +
+				(Math.random() + 1).toString(36).substring(7) +
+				(Math.random() + 1).toString(36).substring(7) +
+				(Math.random() + 1).toString(36).substring(7);
+
+			//Use the mv() method to place the file in upload directory (i.e. "uploads")
+			const path = `./uploads/${randomString}-${uploadedFile.name}`;
+			await uploadedFile.mv(path);
+
+			// scheduling delete
+			setTimeout(() => {
+				fs.rmSync(path);
+			}, 1000 * 5);
+
+			// ocr
+			tesseract
+				.recognize(path, { lang: "eng" })
+				.then((text) => {
+					console.log("Result:", text);
+					res.json({ text });
+				})
+				.catch((error) => {
+					console.log(error.message);
+					res.status(500).json({ msg: error.message });
+				});
+		}
+	} catch (err) {
+		res.status(500).send(err);
 	}
-	// } catch (err) {
-	// 	res.status(500).send(err);
-	// }
 });
 
 // app.get("/download/:filename", (req, res) => {
--- frontend/android/app/src/main/java/com/aryansdomain/ocrapp/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.aryansdomain.ocrapp;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
